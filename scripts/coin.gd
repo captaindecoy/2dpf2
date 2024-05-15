bf6910892a6fab1334dd72e8c38bfd9efd4b65bc
Peter Maricle <pmaricle@gmail.com>
2024-05-15 09:27:13 -0700

    Added particles to coin when collected.

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,8 +3,11 @@ extends Area2D
 @onready var game_manager = %GameManager
 @onready var pickup_sound = $PickupSound
 @onready var animation_player = $AnimationPlayer
+@onready var cpu_particles_2d = $CPUParticles2D
+
 
 func _on_body_entered(_body):
 	#pickup_sound.play()
+	cpu_particles_2d.emitting = true
 	game_manager.add_point()
 	animation_player.play("pickup")
