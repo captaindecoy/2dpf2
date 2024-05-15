1e84391cbbaa7a3c9e0fae3e1b50f23f508de4bd
Peter Maricle <pmaricle@gmail.com>
2024-05-15 14:31:15 -0700

    Added levels and some other smaller configuration

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,8 +1,19 @@
 extends Node
 
-var score = 0
-@onready var score_label = $ScoreLabel
+var score:int = 0
+var current_scene:Node
+@onready var score_label:Label = $ScoreLabel
+@export var my_array:Array[PackedScene]
 
+func _ready():
+	#print(my_array[0].resource_path)
+	current_scene = my_array[0].instantiate()
+	add_child(current_scene)
+	
 func add_point():
 	score += 1
 	score_label.text = "You have collected " + str(score) + " coins!"
+	
+func reload_level():
+	remove_child(current_scene)
+	add_child(current_scene)
