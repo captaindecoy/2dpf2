7557a722ad95b31aaffd09b6953ac2db92a55ff6
Peter Maricle <pmaricle@gmail.com>
2024-05-15 20:04:22 -0700

    Renamed level array var and added back in levels

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,15 +4,15 @@ var score:int = 0
 var current_scene:Node
 var level_index:int = -1
 @onready var score_label:Label = $ScoreLabel
-@export var my_array:Array[PackedScene]
+@export var levels_array:Array[PackedScene]
 
 func _ready():
 	call_deferred("next_level")
-	#get_tree().change_scene_to_packed(my_array[level_index])
+	#get_tree().change_scene_to_packed(levels_array[level_index])
 	#pass
-	#print(my_array[0].resource_path)
+	#print(levels_array[0].resource_path)
 	
-	#current_scene = my_array[0].instantiate()
+	#current_scene = levels_array[0].instantiate()
 	#add_child(current_scene)
 	#var loading = get_node("/LoadingScene")
 	#remove_child(get_tree().current_scene)
@@ -27,7 +27,8 @@ func reload_level():
 	
 func next_level():
 	level_index += 1
-	get_tree().change_scene_to_packed(my_array[level_index])
+	get_tree().change_scene_to_packed(levels_array[level_index])
+	print(levels_array[level_index].resource_path)
 	
 #func change_scene_deferred():
-#	get_tree().change_scene_to_packed(my_array[level_index])
+#	get_tree().change_scene_to_packed(levels_array[level_index])
