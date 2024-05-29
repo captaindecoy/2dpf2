0d27477bb8f13d83bb368dcfbebdb5f7866d49ac
Peter Maricle <pmaricle@gmail.com>
2024-05-29 12:44:43 -0700

    - Fine tuning question block raycasts

--- scripts/question_block.gd
+++ scripts/question_block.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 @onready var left_ray_cast_2d = $LeftRayCast2D
+@onready var middle_ray_cast_2d = $MiddleRayCast2D
 @onready var right_ray_cast_2d = $RightRayCast2D
 
 
@@ -10,7 +11,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if left_ray_cast_2d.is_colliding() or right_ray_cast_2d.is_colliding():
+	if left_ray_cast_2d.is_colliding() or middle_ray_cast_2d.is_colliding() or right_ray_cast_2d.is_colliding():
 		print("Ray collided!")
 		queue_free()
 	pass
